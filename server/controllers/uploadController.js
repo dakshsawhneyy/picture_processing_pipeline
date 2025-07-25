644f2347a8f251714d33da856fe39928def2cc98
dakshsawhneyy <darkstardaksh@gmail.com>
2025-07-25 13:44:49 +0000

    Completed upload controller

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,15 +1,15 @@
-import processing_queue from "../queues/producer";
+import processing_queue from "../queues/producer.js";
 
 const uploadController = async(req,res) => {
     try {
-        const { originalName, path } = req.file;
+        const { originalname, path } = req.file;    // it comes from multer
         
         const filePath = req.file.path // s3://picture-processing-pipeline/image.jpg
 
-        const job = processing_queue.add('Image Processing', {
+        const job = await processing_queue.add('Image Processing', {
             imageID: Math.floor(Math.random() * 1000000), // random image ID of 1-6 digits
             path: filePath,
-            fileName: originalName
+            fileName: originalname
         })
 
         res.status(200).json({success: true, message: 'Job Submitted', JobID: job.id})
@@ -22,7 +22,26 @@ const uploadController = async(req,res) => {
 
 const getJobStatus = async(req,res) => {
     try {
-        
+        const JobID = req.params.id;    // it comes from url
+
+        const job = await processing_queue.getJob(JobID)
+
+        // Base Case
+        if (!job){
+            return res.status(404).json({success:false,message:'Job Not Found'})
+        }
+
+        const state = await job.state();
+        const result = await job.returnvalue; 
+
+        res.json({
+            success: true,
+            message: 'Job Status Retrieved',
+            JobID: job.id,
+            state: state,
+            result: result
+        });
+
     } catch (error) {
         console.log(error);
         res.json({success:false,message:error.message})
