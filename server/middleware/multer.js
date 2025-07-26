968565617091d6ebd947d378add9c9c2c26518d4
dakshsawhneyy <darkstardaksh@gmail.com>
2025-07-26 11:00:04 +0000

    updated multer

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,8 +1,9 @@
 const multer = require('multer')
 
+// This tells multer to store these files on local storage instead of on cloud or any other location
 const storage = multer.diskStorage({
-    destination:function(req, file, callback){
-        callback(null, 'uploads/')  // save it to uploads folder
+    destination:function(req, file, callback){      // stats where to store file
+        callback(null, 'server/uploads/')  // save it to uploads folder
     },
     filename: function(req, file, callback){
         callback(null, file.originalname);
