23fc9ea92c9f6c9b6a7d6abec468e71aca6cc3e4
Daksh Sawhney <darkstardaksh@gmail.com>
2025-07-27 19:26:11 +0530

    Starting Frontend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const uploadRoute = require('./routes/uploadRoute') 
-const path = require('path')
-const Arena = require('bull-arena')
-const basicAuth = require('express-basic-auth')
-const { Queue } = require('bullmq')
-require('dotenv').config
+const express = require('express');
+const uploadRoute = require('./routes/uploadRoute');
+const path = require('path');
+const basicAuth = require('express-basic-auth');
+require('dotenv').config();
 
 const app = express();
 
@@ -12,31 +10,6 @@ app.use('/api', uploadRoute);
 
 // join path where you are running, it should be opened correctly where (__dirname) -> server.js is placed
 app.use('/processed', express.static(path.join(__dirname, 'processed')))  // if someone opens /processed, serve them files in processed folder
-
-// Api calling for Bull Arena - Dashboard for our queues
-app.use('/arena', 
-    basicAuth({     // this acts as a middleware in between route and Arena 
-        users: { [process.env.BASIC_AUTH_ADMIN]: process.env.BASIC_AUTH_ADMIN },      // username: admin, password: is in env
-        challenge: true     // causes browser to show login prompt
-    }),
-    Arena({
-        BullMQ: Queue,
-        queues: [
-            {
-                type: 'bullmq',     // queue type
-                name: 'image-processing-q',     // name of the queue
-                hostId: 'Image Processing Queue',   // Label shown in UI
-                redis:{
-                    host: '127.0.0.1',
-                    port: 6379
-                }
-            }
-        ]
-    },{
-        basePath: '/arena',     // Arena UI will be served at this path
-        disableListen: true     // Don't start its own server; the Arena
-    })
-)
 
 app.listen(9000, () => {
     console.log('Server is Running');
